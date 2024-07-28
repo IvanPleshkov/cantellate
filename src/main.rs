abd89c77155bbadd3d98b1bef571a3ad0ae5f77a
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-07-29 00:19:29 +0200

    process directory as an input

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod cantellate;
 mod mesh;
 mod vec3;
 
+use std::path::PathBuf;
+
 use cantellate::cantellate;
 use clap::Parser;
 use mesh::Mesh;
@@ -12,10 +14,12 @@ use num_traits::{Float, FromPrimitive, ToPrimitive};
 #[clap(about)]
 pub struct Args {
     /// Input `.obj` file.
+    /// If input is a directory, all `.obj` files in the directory will be processed.
     #[arg(short, long)]
     input: String,
 
     /// Output `.obj` file.
+    /// If input is a directory, all output files will be saved in this directory.
     #[arg(short, long)]
     output: String,
 
@@ -40,18 +44,47 @@ fn main() {
     // parse command line arguments
     let args = Args::parse();
 
-    if args.double {
-        run::<f64>(args);
-    } else {
-        run::<f32>(args);
+    let input_path: PathBuf = args.input.clone().into();
+    if input_path.is_file() {
+        if args.double {
+            run::<f64>(&args);
+        } else {
+            run::<f32>(&args);
+        }
+    } else if input_path.is_dir() {
+        let output_dir = PathBuf::from(&args.output);
+        if output_dir.exists() {
+            std::fs::remove_dir_all(&args.output).unwrap();
+        }
+
+        // if input is a directory, iterate over all files in the directory
+        for file in input_path.read_dir().unwrap() {
+            let file = file.unwrap();
+            if file.path().is_file() && file.path().extension().and_then(|e| e.to_str()) == Some("obj") {
+                let input = file.path().to_str().unwrap().to_string();
+                let output = output_dir.join(file.file_name()).to_str().unwrap().to_string();
+                let args = Args {
+                    input,
+                    output,
+                    ..args.clone()
+                };
+                if args.double {
+                    run::<f64>(&args);
+                } else {
+                    run::<f32>(&args);
+                }
+            }
+        }
     }
+
 }
 
 // Run the demo.
-fn run<N>(args: Args)
+fn run<N>(args: &Args)
 where
     N: Float + ToPrimitive + FromPrimitive + Default,
 {
+    println!("Input mesh: {}, output {}", args.input, args.output);
     // load the input mesh
     let mesh = Mesh::<N>::load_obj(&args.input).unwrap();
 
@@ -70,6 +103,13 @@ where
             result.vertices.len()
         );
         result
+    });
+
+    let output_path: PathBuf = args.output.clone().into();
+    output_path.parent().map(|output_dir| {
+        if !output_dir.exists() {
+            std::fs::create_dir_all(output_dir).unwrap();
+        }
     });
 
     // save the output mesh
